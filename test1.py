ede711b030715712d0a6fac3719f24f86cc2720b
TuanNguyen <nguyenanhtuan.developer.mail@gmail.com>
2022-03-03 14:12:28 +0700

    update test1.py

--- test1.py
+++ test1.py
@@ -4,10 +4,13 @@ import numpy as np
 
 env_var = os.environ
 
-def main():
+def test1():
     a = 10
     b = 20 
     print(a+b)
 
+def main():
+    test1()
+    print('Now run through test1')
 if __name__=="__main__":
     main()
